c81372e8bba0b2fd2b75848d6793cac2dbdbf4b6
Muhammad Mahad <mahadtxt@gmail.com>
2024-07-03 20:19:19 +0500

    Add Basic Error Adaption feature
    * Add CLI parsing feature using clap
    * Working on simplest case by converting `ERROR` directive to `dg-error` directive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use anyhow::Result;
+use clap::Parser;
+use std::{fs, path, process};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short = 'f', long = "file")]
+    file: path::PathBuf,
+}
+
+const DG_ERROR: &str = "//~^ ERROR ";
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = try_parse() {
+        eprintln!("Error: {:?}", error);
+        process::exit(1);
+    }
+}
+
+fn try_parse() -> Result<()> {
+    let args = Cli::parse();
+
+    if !args.file.exists() {
+        panic!("File {:?} not found", args.file);
+    }
+
+    let code = fs::read_to_string(&args.file).expect("Failed to read file");
+
+    let new_code = transform_code(&code, DG_ERROR);
+
+    fs::remove_file(&args.file).expect("Failed to delete file");
+    fs::write(&args.file, new_code.join("\n")).expect("Failed to write to file");
+
+    Ok(())
+}
+
+fn transform_code(code: &String, dg_directive: &str) -> Vec<String> {
+    let mut new_code = Vec::new();
+
+    for line in code.lines() {
+        if line.contains(dg_directive) {
+            // replace the rust directive to dejagnu directive
+            // TODO: Add more directive relative to rustc
+            let new_line = line.replace(dg_directive, "// { dg-error \"");
+
+            // format the line according to dejagnu format
+            let new_line = format!("{}\" }}", new_line);
+            new_code.push(new_line);
+        } else {
+            new_code.push(line.to_string());
+        }
+    }
+    new_code
+}
 }
