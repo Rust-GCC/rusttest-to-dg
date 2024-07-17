c9b3584f385fed693905575b9b3e674c410e8db8
Muhammad Mahad <mahadtxt@gmail.com>
2024-07-18 00:18:45 +0500

    Refactor transform_code into new file
    - Updated the return type from `Vec<String>` to `String`
    
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod cli;
+mod transform;
 
 use anyhow::{Context, Result};
 use clap::Parser;
@@ -17,43 +18,15 @@ fn try_parse() -> Result<()> {
     let code = fs::read_to_string(&args.source_file)
         .with_context(|| format!("could not read file `{}`", args.source_file.display()))?;
 
-    let new_code = transform_code(&code, RUSTTEST_ERROR);
+    let new_code = transform::transform_code(&code).with_context(|| {
+        format!(
+            "could not transform code from file `{}`",
+            args.source_file.display()
+        )
+    })?;
 
     fs::remove_file(&args.source_file)?;
     fs::write(&args.source_file, new_code.join("\n"))?;
 
     Ok(())
-}
-
-/// This function takes the rust code and rust directive
-/// and returns the code with dejagnu directive
-fn transform_code(code: &str, rust_directive: &str) -> Vec<String> {
-    let mut new_code = Vec::new();
-
-    for line in code.lines() {
-        if line.contains(rust_directive) {
-            // replace the rust directive to dejagnu directive
-            // TODO: Add more directive relative to rustc
-            let new_line = line.replace(rust_directive, DG_ERROR);
-
-            // format the line according to dejagnu format
-            let new_line = format!("{}\" }}", new_line);
-            new_code.push(new_line);
-        } else {
-            new_code.push(line.to_string());
-        }
-    }
-    new_code
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_transform_code() {
-        let dg_msg = "// { dg-error \"expected one of `:`, `@`, or `|`, found `)`\" }";
-        let rust_msg = "//~^ ERROR expected one of `:`, `@`, or `|`, found `)`";
-        assert_eq!(transform_code(rust_msg, RUSTTEST_ERROR), vec![dg_msg]);
-    }
 }
---
+++ src/transform.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+
+// TODO: Add more directive relative to rustc and DejaGnu
+pub const RUSTTEST_ERROR: &'static str = "//~^ ERROR ";
+pub const DG_ERROR: &'static str = "// { dg-error \"";
+
+/// This function takes the rust code and rust directive
+/// and returns the code with DejaGnu directive
+pub fn transform_code(code: &str) -> Result<String> {
+    let new_code = code
+        .lines()
+        .map(|line| {
+            if line.contains(RUSTTEST_ERROR) {
+                transform_line(line, RUSTTEST_ERROR, DG_ERROR)
+            } else {
+                line.to_string()
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    Ok(new_code)
+}
+
+fn transform_line(line: &str, rust_directive: &str, dejagnu_directive: &str) -> String {
+    let new_line = line.replace(rust_directive, dejagnu_directive);
+    // format the line according to DejaGnu format
+    format!("{}\" }}", new_line)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transform() {
+        let dg_msg = "// { dg-error \"expected one of `:`, `@`, or `|`, found `)`\" }";
+        let rust_msg = "//~^ ERROR expected one of `:`, `@`, or `|`, found `)`";
+        assert_eq!(transform_code(rust_msg).unwrap(), dg_msg);
+    }
+
+    #[test]
+    fn test_transform_line() {
+        let dg_msg = "// { dg-error \"expected one of `:`, `@`, or `|`, found `)`\" }";
+        let rust_msg = "//~^ ERROR expected one of `:`, `@`, or `|`, found `)`";
+        assert_eq!(transform_line(rust_msg, RUSTTEST_ERROR, DG_ERROR), dg_msg);
+    }
+}
