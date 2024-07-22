fcf30204a5b6836e2856ba30fa2c76041777b28e
Muhammad Mahad <mahadtxt@gmail.com>
2024-07-22 12:03:30 +0500

    Add logic to load_errors into `Error` struct
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,3 @@
-use std::{fmt, str::FromStr};
 
 // https://rustc-dev-guide.rust-lang.org/tests/ui.html#error-levels
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -42,6 +41,114 @@ impl fmt::Display for RustcErrorKind {
             RustcErrorKind::Warning => write!(f, "warning"),
         }
     }
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub line_num: usize,
+    /// We also need to take into account the relative line number.
+    /// if the error is on the previous line, then it's value is `1`.
+    /// if the error is on the same line, then it's value is `0`.
+    /// if the error is on the next line, then it's value is `-1`.
+    pub relative_line_num: i32,
+
+    /// What kind of message we expect (e.g., warning, error, suggestion).
+    /// `None` if not specified or unknown message kind.
+    pub kind: Option<RustcErrorKind>,
+    pub msg: String,
+    pub error_code: Option<String>,
+#[derive(PartialEq, Debug)]
+enum WhichLine {
+    ThisLine,
+    FollowPrevious(usize),
+    AdjustBackward(usize),
+}
+
+pub fn load_error(text_file: &str) -> Vec<Error> {
+    let mut last_unfollow_error = None;
+    let mut errors = Vec::new();
+
+    for (line_num, line) in text_file.lines().enumerate() {
+        if let Some((which, error)) = parse_expected(last_unfollow_error, line_num + 1, line) {
+            match which {
+                FollowPrevious(_) => {}
+                _ => last_unfollow_error = Some(line_num),
+            }
+            errors.push(error);
+        }
+    }
+
+    errors
+}
+
+fn parse_expected(
+    last_nonfollow_error: Option<usize>,
+    line_num: usize,
+    line: &str,
+) -> Option<(WhichLine, Error)> {
+    // Matches comments like:
+    //     //~
+    //     //~|
+    //     //~^
+    //     //~^^^^^
+    static RE: OnceLock<Regex> = OnceLock::new();
+
+    let captures = RE
+        .get_or_init(|| Regex::new(r"//(?:\[(?P<revs>[\w\-,]+)])?~(?P<adjust>\||\^*)").unwrap())
+        .captures(line)?;
+
+    let (follow, adjusts) = match &captures["adjust"] {
+        "|" => (true, 0),
+        circumflexes => (false, circumflexes.len()),
+    };
+
+    // Get the part of the comment after the sigil (e.g. `~^^` or ~|).
+    let whole_match = captures.get(0).unwrap();
+    let (_, mut msg) = line.split_at(whole_match.end());
+
+    let first_word = msg
+        .split_whitespace()
+        .next()
+        .expect("Encountered unexpected empty comment");
+
+    // If we find `//~ ERROR foo` or something like that, skip the first word.
+    let kind = first_word.parse::<RustcErrorKind>().ok();
+    if kind.is_some() {
+        msg = msg.trim_start().split_at(first_word.len()).1;
+    }
+
+    let msg = msg.trim().to_owned();
+
+    let mut relative_line_num = line_num as i32;
+    let (which, line_num) = if follow {
+        assert_eq!(adjusts, 0, "use either //~| or //~^, not both.");
+        let line_num = last_nonfollow_error.expect(
+            "encountered //~| without \
+             preceding //~^ line.",
+        );
+        relative_line_num = (line_num as i32) - relative_line_num;
+        (FollowPrevious(line_num), line_num)
+    } else {
+        let which = if adjusts > 0 {
+            AdjustBackward(adjusts)
+        } else {
+            ThisLine
+        };
+        let line_num = line_num - adjusts;
+        relative_line_num = -(adjusts as i32);
+        (which, line_num)
+    };
+
+    Some((
+        which,
+        Error {
+            line_num,
+            kind,
+            msg,
+            error_code: "E0000".to_owned().into(),
+            relative_line_num,
+        },
+    ))
 }
 
 #[cfg(test)]
