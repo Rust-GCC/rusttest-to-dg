54a42dfc5cfdb2d4820916339fb35cced682e2ef
Muhammad Mahad <mahadtxt@gmail.com>
2024-08-11 18:08:53 +0500

    Add `regex!` Macro
    Add `regex` and exported it for other files
    
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -4,6 +4,14 @@ use regex::Regex;
 
 use self::WhichLine::*;
 
+// https://docs.rs/once_cell/1.19.0/once_cell/#lazily-compiled-regex
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
 // https://rustc-dev-guide.rust-lang.org/tests/ui.html#error-levels
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum RustcErrorKind {
