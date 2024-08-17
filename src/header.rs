e180b3b50baeefa66ab362c233dd9e13d4fb84e2
Muhammad Mahad <mahadtxt@gmail.com>
2024-08-17 13:40:11 +0500

    Add function to store `HeaderLine` info
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/header.rs
+++ src/header.rs
@@ -12,6 +12,25 @@ pub struct HeaderLine<'ln> {
 }
 
 
+fn add_additional_options(code: &str, line_number: usize) -> Option<HeaderLine> {
+    //TODO: If we know the file extension, then update this to
+    // let comment = if testfile.extension().is_some_and(|e| e == "rs") { "//@" } else { "#" };
+    let comment = "//@";
+
+    if let Some((_header_revision, non_revisioned_directive_line)) = line_directive(comment, code) {
+        // The non_revisioned_directive_line is the directive without the "//@" prefix
+        let edition = parse_edition(non_revisioned_directive_line);
+        edition.as_ref()?;
+        Some(HeaderLine {
+            line_number: line_number + 1, // 1 based-indexed instead of zero based
+            _directive: "edition",
+            dejagnu_header: to_dejagnu_edition(edition.unwrap().as_str()),
+        })
+    } else {
+        None
+    }
+}
+
 fn line_directive<'line>(
     comment: &str,
     original_line: &'line str,
