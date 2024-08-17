0a53af75b5c81ff72974f6066fb5de9da530b863
Muhammad Mahad <mahadtxt@gmail.com>
2024-08-17 13:33:08 +0500

    Add `HeaderLine` struct to store header info
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,13 @@
 //! This module contains the logic for parsing rust test headers
 //! See [rustc dev guide](https://rustc-dev-guide.rust-lang.org/tests/headers.html#test-headers)
 
+#[derive(Debug)]
+pub struct HeaderLine<'ln> {
+    pub line_number: usize,
+    /// The main part of the header directive, after removing the comment prefix
+    /// and the optional revision specifier.
+    pub _directive: &'ln str,
+    /// DejaGnu formatted header line
+    pub dejagnu_header: String,
+}
+
