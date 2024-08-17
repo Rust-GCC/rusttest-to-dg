2a84c7176359b41937681c08a26b4ecbbf2efbe4
Muhammad Mahad <mahadtxt@gmail.com>
2024-08-17 13:34:38 +0500

    Add `parse_edition` functions
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- src/header.rs
+++ src/header.rs
@@ -11,3 +11,18 @@ pub struct HeaderLine<'ln> {
     pub dejagnu_header: String,
 }
 
+fn parse_edition(line: &str) -> Option<String> {
+    parse_name_value_directive(line, "edition")
+}
+
+fn parse_name_value_directive(line: &str, directive: &str) -> Option<String> {
+    let colon = directive.len();
+
+    if line.starts_with(directive) && line.as_bytes().get(colon) == Some(&b':') {
+        let value = line[(colon + 1)..].to_owned();
+        Some(value)
+    } else {
+        None
+    }
+}
+
